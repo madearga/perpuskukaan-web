f8f2f62ee349f4d19999e4adf827c267db6f34a3
madearga <madearga.work@gmail.com>
2026-05-01 23:59:24 +0700

    fix(build): uncomment authClient import in SignIn.tsx

--- src/app/(unauth)/sign-in/SignIn.tsx
+++ src/app/(unauth)/sign-in/SignIn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { authClient } from "@/lib/auth-client"; // TODO: re-enable after auth config
+import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
